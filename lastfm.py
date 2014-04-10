e7d1a047e419c782df95f07e89bfc1c9b670a969
Ricardo L. Dani <ricardodani@gmail.com>
2014-04-10 01:11:37 -0300

    Second commit.

--- lastfm.py
+++ lastfm.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import io
 import lastfmapi
 import memcache
+import requests
+from PIL import Image
 
 LASTFM_API_KEY = 'c971171e6f5976ddae7717ce53ea2ee6'
 MEMCACHE_ADDRESS = '127.0.0.1:11211'
@@ -14,11 +17,11 @@ def _get_user_top_albums_response(**kwargs):
     '''Returns the response of the api album.getTopAlbums call
     '''
 
-    cache_key = 'topalbums|{user}|{period}'.format(**kwargs)
+    cache_key = 'UsTopAlb|{user}|{period}'.format(**kwargs)
 
     res = mc.get(cache_key)
     if not res:
-        res = api.user_getTopAlbums(**kwargs)['topalbums']['album'][:50]
+        res = api.user_getTopAlbums(**kwargs)['topalbums']['album']
         mc.set(cache_key, res)
     return res
 
@@ -31,9 +34,26 @@ def get_user_top_albums_images(user, period='overall'):
         # image qualities -> 0 - 3
         return album['image'][2]['#text']
 
-    return map(get_img,
-               _get_user_top_albums_response(user=user, period=period))
+    images_url = _get_user_top_albums_response(user=user, period=period, limit=60)
+    return filter(lambda x: 'default_album_medium.png' not in x,
+                  map(get_img, images_url))[:50]
 
-# available periods: overall (default) | 7day | 1month | 3month | 6month | 12month
-# just pass in second argument
-print get_user_top_albums_images('ricardodani', '1month')
+
+def create_image_mosaic(image_url_list):
+    img = Image.new('RGB', (1260, 1260/2))
+    x, y = 0, 0
+    for i in range(len(image_url_list)):
+        res = requests.get(image_url_list[i])
+        stream = io.BytesIO(res.content)
+        alb = Image.open(stream)
+        img.paste(alb, (y*126, x*126))
+        if (i+1) % 10 == 0:
+            x += 1
+            y = 0
+        else:
+            y += 1
+    img.save('mosaic.png')
+
+
+
+create_image_mosaic(get_user_top_albums_images('ricardodani', '1month'))
